7a1d5e5e7035429e03c6e8fa5890eeea3aff3f1a
ezequias costa <bieldotor@gmail.com>
2026-09-13 19:36:11 +0000

    subindo arquivos que tava faltando

---
+++ app/(auth)/Login/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+export default function Login() {
+    const [email,setEmail] = useState("")
+    const [senha,setSenha] = useState("")
+
+    const entrar = () => setEmail === email && setSenha === senha ? src="./page.tsx"  ! alert("erro")
+   
+    return (
+        <div className="w-full h-screen text-center flex flex-col items-center gap-6">
+            <h1>Login</h1>
+            <h2>Banco do pobre</h2>
+            <input type="email" name="email" id="email"
+             placeholder="Digite seu email" onChange={(e)=>setEmail(e.target.value)} />
+
+            <input type="password" name="senha" id="senha"
+             placeholder="Digite sua senha" onChange={(e)=>setSenha(e.target.value)}/>
+
+            <button onClick={entrar}>Entrar</button>
+        </div>
+    )
+}
